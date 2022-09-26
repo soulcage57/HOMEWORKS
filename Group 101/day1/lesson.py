68260baf1ac7161c76604f0955470fd3fe32a7f9
soulcage57 <bekadidebashvili@yahoo.com>
2022-09-26 12:01:36 +0400

    first commit

---
+++ Group 101/day1/lesson.py
@@ -0,0 +1,22 @@
+name = "Beka"
+surname = "Didebashvili"
+nikname = "soulcage"
+age = 31
+height = 170.5
+weight = 71.5
+nationality = "saqartvelos moqalaqe"
+sacxovrebeli_adgili = "Qalaq Tbilisshi"
+saswavlebeli = "Grenas qselur akademiashi"
+hobi = "kodis wera"
+job_position = "Network Administratorad"
+
+
+print("Me var" " " + name +" " + surname + " da var " + str(age) + " " "wlis," " " + "chemi simagle aris" " "
++ str(height) + " " "metri,"  + " " "xolo wona=" + str(weight)+"."  + " " "var" " " + str(nationality) +" " "da vcxovrob" " "
++ str(sacxovrebeli_adgili)+"." +  " " "vswavlobdi" " " + str(saswavlebeli)+" " +
+ "da" " " "vmushaob" " "+ str(job_position)+"." + "minda rom chemi hobi gaxdes" " " + str(hobi)
+) 
+
+
+
+
---
+++ Group 101/day2/lesson2.py
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+name = "beka didebashili"
+age = 31
+height = 170.5
+
+print(len(name))
+if "beka" in name:
+    print(name.upper())
+
+
+print(name.replace (" ",""))
+
+print(name.replace("beka","Tako"))
+
+
+if "b" in name:
+    print(name.upper())
+
+print(name.capitalize())
+
+if "beka" in name:
+    print(name.replace("beka","eka"))
+
+
+
+
+
+
+
+
+
