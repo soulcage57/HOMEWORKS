53760c1c5927c70eb33cdea82c6b6da94a9bf2e4
soulcage57 <bekadidebashvili@yahoo.com>
2022-09-28 11:30:26 +0400

    first commit

--- Group 101/day2/lesson2.py
+++ Group 101/day2/lesson2.py
@@ -1,32 +1,29 @@
 
 
+# name = "beka didebashili"
+# age = 31
+# height = 170.5
+
+# print(len(name))
+# if "beka" in name:
+#     print(name.upper())
 
 
+# print(name.replace (" ",""))
+
+# print(name.replace("beka","Tako"))
 
 
-name = "beka didebashili"
-age = 31
-height = 170.5
+# if "b" in name:
+#     print(name.upper())
 
-print(len(name))
-if "beka" in name:
-    print(name.upper())
+# print(name.capitalize())
 
+# if "beka" in name:
+#     print(name.replace("beka","eka"))
 
-print(name.replace (" ",""))
-
-print(name.replace("beka","Tako"))
-
-
-if "b" in name:
-    print(name.upper())
-
-print(name.capitalize())
-
-if "beka" in name:
-    print(name.replace("beka","eka"))
-
-
+# name= "Conference Hall"
+# print(name.upper())
 
 
 
---
+++ Group 101/day3/lesson3.py
@@ -0,0 +1,33 @@
+     
+# name = input("enter your name:")
+# surname = input("enter your surname: ")
+# age = int(input ("enter your age: "))
+
+# print("chemi saxelia {} gvari {} da chemi asakia {}".format(name,surname,age))
+
+# age +=3
+# print( "gavida sami weli da me gavxdi {} wlis".format(age))
+
+
+
+
+
+
+
+num1 = int(input("type number first: "))
+num2 = int(input("type number two: "))
+num3 = int(input("type number three: "))
+
+
+sum = 0
+if num1 % 2 ==1: 
+    sum += num1
+
+if num2 % 2 ==1:
+    sum += num2
+if num3 % 2 ==1:
+    sum += num3
+        
+print(sum)     
+      
+
